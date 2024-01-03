101833f791474213f6612bc944d8afeaf6287c92
FongFox <phong.tgn21022002.coder@gmail.com>
2024-01-03 12:27:52 +0700

    update okta config

--- front-end/PLibrary/src/lib/oktaConfig.ts
+++ front-end/PLibrary/src/lib/oktaConfig.ts
@@ -1,11 +1,13 @@
 export const oktaConfig = {
     // Thêm thông tin okta
-    // clientId: '<CLIENTID HERE>',
-    // issuer: 'https://dev-<DEV ID>.okta.com/oauth2/default',
-    clientId: '0oadtu7l2xowPDosk5d7',
-    issuer: 'https://dev-47183227.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
-    scopes: ['openid', 'profile', 'email'],
+    //clientId: '<CLIENTID HERE>',
+    //issuer: 'https://dev-<DEV ID>.okta.com/oauth2/default',
+    
+	//clientId: '0oadtu7l2xowPDosk5d7',
+    //issuer: 'https://dev-47183227.okta.com/oauth2/default',
+	
+	redirectUri: 'http://localhost:3000/login/callback',
+	scopes: ['openid', 'profile', 'email'],
     pkce: true,
-    disableHttpsCheck: true,
+    disableHttpsCheck: true
 }
